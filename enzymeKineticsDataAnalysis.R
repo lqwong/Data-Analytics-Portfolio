3a21b8485cf9a4c144977be30b6ebc7f08a18284
lqwong <127452284+lqwong@users.noreply.github.com>
2023-03-13 15:55:39 -0700

    Update enzymeKineticsDataAnalysis.R

--- enzymeKineticsDataAnalysis.R
+++ enzymeKineticsDataAnalysis.R
@@ -31,9 +31,9 @@ stripchart(Rate~Temperature,
            method = "jitter",
            pch = 1,
            ylim = c(0,3.5))
-segments(x0 = .9, y0 = 0.25, x1 = 2.0, y1 = 0.25, lwd=3)
-segments(x0 = 2.8, y0 = 0.25, x1 = 5.2, y1 = 0.25, lwd=3)
-segments(x0 = 5.8, y0 = 0.25, x1 = 7.2, y1 = 0.25, lwd=3)
+segments(x0 = .9, y0 = 0.25, x1 = 2.0, y1 = 0.25, lwd=3) #Depicts no significant difference in 20-30 temperature group
+segments(x0 = 2.8, y0 = 0.25, x1 = 5.2, y1 = 0.25, lwd=3) #Depicts no significant difference in 37-40-50 temperature group
+segments(x0 = 5.8, y0 = 0.25, x1 = 7.2, y1 = 0.25, lwd=3) #Depicts no significant difference in 60-70 temperature group
 
 
 
